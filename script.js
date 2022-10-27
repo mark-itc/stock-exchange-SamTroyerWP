9cb41f58f52f35d9dcce40a5cf9cec55656f8977
SamTroyerWP <101744512+SamTroyerWP@users.noreply.github.com>
2022-10-27 23:24:42 +0300

    Stock Exchange Milestone 7

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-marqueeStocks();
 
 const searchButton = document.getElementById('searchButton');
-=======
-const searchButton = document.getElementById('searchButton')
->>>>>>> f7f0bddfe003885946077b85a0bed36fe1bc7d96
 
 searchButton.addEventListener('click', () => {
     document.getElementById("tableResults").innerHTML = '';
@@ -17,11 +12,7 @@ async function searchResults() {
     const url = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query="+ symbol + "&limit=10&exchange=NASDAQ"
     const response = await fetch(url)
     const result = await response.json();
-
-   
-
-<<<<<<< HEAD
-    // companyResults();
+    
     stopSpinner();
     
     const tableResults = document.getElementById("tableResults");
@@ -31,6 +22,8 @@ async function searchResults() {
             let currentResult = searchResults[i];
             tableResults.innerHTML += ('<img src="https://financialmodelingprep.com/images-New-jpg/' + currentResult.symbol + '.jpg"> ' + "<a href='company.html?symbol=" + currentResult.symbol +"'>"  + currentResult.name  + ' </a>' + '(' + currentResult.symbol + ')');
 
+
+            
             const url2 = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/" + currentResult.symbol;
             const response2 = await fetch(url2);
             const result2 = await response2.json();
@@ -45,68 +38,23 @@ async function searchResults() {
             //     tableResults.innerHTML = ' (' + compResults.profile.changesPercentage[i] + '%)' + '<hr>';
             //     tableResults.style.color = "red";
             // }
+            highlight();
+
         }
     } catch (error) {
         console.error(error);
     }
 
-    // async function companyResults() {
-    //     const url2 = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/";
-    //     const response2 = await fetch(url2 + currentResult.symbol);
-    //     console.log(response2)
-    //     const result2 = await response2.json();
-    //     let compResults = result2;
-    //             const tableResults = document.getElementById("tableResults");
-
-    //     try {
-    //         for (let i = 0; i < compResults.length; i++) {
-    //             let currentResult = compResults[i];
-    //             tableResults.innerHTML() += '$' + currentResult.profile.price;
-    //         }
-    //     } catch (error) {
-    //     console.error(error);
-    //     }
-    // }
-=======
-function searchResults() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-
-        stopSpinner();
-        
-        let text = this.response;
-        let result = "";
-        
-        JSON.parse(text).forEach(value => result += "<a href='company.html?symbol=" + value.symbol +"'>" + value.name + "  " + '(' + value.symbol + ') </a>  <hr>')
-        
-
-        document.getElementById("tableResults").innerHTML = result;
-        }
-    };
-
-    xmlhttp.open("GET", "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=AA&limit=10&exchange=NASDAQ", true);
-    xmlhttp.send();
->>>>>>> f7f0bddfe003885946077b85a0bed36fe1bc7d96
 }
 
-
-            
-async function marqueeStocks() {
-    const url = "https://financialmodelingprep.com/api/v3/stock/list?apikey=63ce29e954a5731b77ccf40f124b41b2"
-    const response = await fetch(url);
-    const result = await response.json();
-    const marqueeContainer = document.getElementById("marqueeNav");
-    let marqueeResult = result;
-    try {
-        for (let i = 45000; i < marqueeResult.length; i++) {
-            let currentResult = marqueeResult[i];
-            marqueeContainer.innerHTML += ( currentResult.symbol + ': ' + '<span>' + '$' + currentResult.price + '</span>' + ' ');        
-        }
-    } catch (error) {
-        console.error(error);
-    }  
-}      
+function highlight() {
+    let ob = new Mark(document.querySelector(".table-results"));
+    ob.unmark();
+    ob.mark(
+        document.getElementById("searchTerm").value,
+        {className: 'a'}
+    );
+}
 
 function addSpinner() {
     document.getElementsByClassName('loader')[0].style.display = 'block';
