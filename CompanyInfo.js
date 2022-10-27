83033bceb1ade2e1301b7e2ea13d21ddc43b3b55
SamTroyerWP <101744512+SamTroyerWP@users.noreply.github.com>
2022-10-27 18:44:49 +0300

    Stock Exchange Milestone 3-4

--- CompanyInfo.js
+++ CompanyInfo.js
@@ -1,85 +1,177 @@
-(async function () {
-    const params = new URLSearchParams(location.search);
-    const symbol = params.get('symbol');
-    const compInfo = new CompanyInfo(document.getElementById('compInfo'), symbol);
-    await compInfo.load();
-    await compInfo.addChart();
-})();
+const myKeysValues = window.location.search;
+const urlParams = new URLSearchParams(myKeysValues);
+const param1 = urlParams.get('symbol');
 
-let companies = [
- CAN = {
-    "name" : "Canaan Inc.",
-    "symbol" : "CAN",
-    "currency" : "USD",
-    "stockExchange" : "NASDAQ Global Market",
-    "exchangeShortName" : "NASDAQ"
-},
 
-PAA = {
-    "symbol":"PAA",
-    "name":"Plains All American Pipeline, L.P.",
-    "currency":"USD",
-    "stockExchange":"NASDAQ Global Select",
-    "exchangeShortName":"NASDAQ"
-},
 
- AAL = {
-    "symbol":"AAL",
-    "name":"American Airlines Group Inc.",
-    "currency":"USD",
-    "stockExchange":"NASDAQ Global Select",
-    "exchangeShortName":"NASDAQ"
-},
+async function companyResults() {
 
- RAAC = {
-    "symbol":"RAAC",
-    "name":"Revolution Acceleration Acquisition Corp",
-    "currency":"USD",
-    "stockExchange":"Nasdaq Global Select",
-    "exchangeShortName":"NASDAQ"
-},
+    const symbol = param1;
+    const url = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/"
+    const response = await fetch(url + symbol);
+    const result = await response.json();
 
- MAAC = {
-    "symbol":"MAAC",
-    "name":"Montes Archimedes Acquisition Corp.",
-    "currency":"USD",
-    "stockExchange":"Nasdaq Capital Market",
-    "exchangeShortName":"NASDAQ"
-},
+    stopSpinner();
+    
+    document.getElementById("compImage").innerHTML = '<img src="' + result.profile.image + '">';
+    document.getElementById("compName").innerHTML = result.profile.companyName + ' (' + result.profile.sector + ')';
+    document.getElementById("compPrice").innerHTML = "Stock Price: $" + result.profile.price;
+    const changesPercentage = document.getElementById("compChanges");
+        if (result.profile.changesPercentage >= 0) {
+            changesPercentage.innerHTML = ' (+' + result.profile.changesPercentage + '%)';
+            changesPercentage.style.color = "green";
+        } else {
+            changesPercentage.innerHTML = ' (' + result.profile.changesPercentage + '%)';
+            changesPercentage.style.color = "red"
+        }
+    document.getElementById("compDescription").innerHTML = result.profile.description;
+};
 
- AAME = {
-    "symbol":"AAME",
-    "name":"Atlantic American Corporation",
-    "currency":"USD",
-    "stockExchange":"NASDAQ Global Market",
-    "exchangeShortName":"NASDAQ"
-},
 
- OHAA = {
-    "symbol":"OHAA",
-    "name":"OPY Acquisition Corp. I",
-    "currency":"USD",
-    "stockExchange":"NASDAQ Global Market",
-    "exchangeShortName":"NASDAQ"
-},
+        async function stockHistoryResults() {
+            const response = await fetch(
+"https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/" + param1 + "?serietype=line");
+            const data = await response.json();
+            length = data.historical.length;
+  
+            labels = [];
+            values = [];
+            for (let i = length - 1; i >= 0; i--) {
+                labels.push(data.historical[i].date);
+                values.push(data.historical[i].close);
+            }
+  
+  
+            new Chart(document.getElementById("stockChart"), {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            backgroundColor: ["#3e95cd"],
+                            data: values
+                        }
+                    ]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    fill: true
+                }
+            });
+  
+        }
 
- MCAA = {
-    "symbol":"MCAA",
-    "name":"Mountain & Co. I Acquisition Corp.",
-    "currency":"USD",
-    "stockExchange":"NASDAQ Global Market",
-    "exchangeShortName":"NASDAQ"
-},
+window.onload = function() {
+    companyResults();
+    addSpinner();
+    stockHistoryResults();
+};
 
- FWAA = {
-    "symbol":"FWAA",
-    "name":"Fifth Wall Acquisition Corp. I",
-    "currency":"USD","stockExchange":"Nasdaq Capital Market",
-    "exchangeShortName":"NASDAQ"
-},
+function addSpinner() {
+    document.getElementsByClassName('loader')[0].style.display = 'block';
+}
 
- PRAA = {
-    "symbol":"PRAA",
-    "name":"PRA Group, Inc.",
-    "currency":"USD",
-    "stockExchange":"NASDAQ Global Select","exchangeShortName":"NASDAQ"}]
+function stopSpinner() {
+    document.getElementsByClassName('loader')[0].style.display = 'none';
+}
+
+// class CompanyInfo { 
+//     constructor(companyObject) { 
+//         this.name = companyObject.Name;
+//         this.symbol = companyObject.Symbol;
+//         this.currency = companyObject.Currency;
+//         this.stockExchange = companyObject.StockExchange;
+//         this.exchangeShortName = companyObject.ExchangeShortName;
+//     }
+// }
+
+//     const CAN = {
+//        "name" : "Canaan Inc.",
+//        "symbol" : "CAN",
+//        "currency" : "USD",
+//        "stockExchange" : "NASDAQ Global Market",
+//        "exchangeShortName" : "NASDAQ"
+//    };
+   
+//    const PAA = {
+//        "symbol":"PAA",
+//        "name":"Plains All American Pipeline, L.P.",
+//        "currency":"USD",
+//        "stockExchange":"NASDAQ Global Select",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const AAL = {
+//        "symbol":"AAL",
+//        "name":"American Airlines Group Inc.",
+//        "currency":"USD",
+//        "stockExchange":"NASDAQ Global Select",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const RAAC = {
+//        "symbol":"RAAC",
+//        "name":"Revolution Acceleration Acquisition Corp",
+//        "currency":"USD",
+//        "stockExchange":"Nasdaq Global Select",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const MAAC = {
+//        "symbol":"MAAC",
+//        "name":"Montes Archimedes Acquisition Corp.",
+//        "currency":"USD",
+//        "stockExchange":"Nasdaq Capital Market",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const AAME = {
+//        "symbol":"AAME",
+//        "name":"Atlantic American Corporation",
+//        "currency":"USD",
+//        "stockExchange":"NASDAQ Global Market",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const OHAA = {
+//        "symbol":"OHAA",
+//        "name":"OPY Acquisition Corp. I",
+//        "currency":"USD",
+//        "stockExchange":"NASDAQ Global Market",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const MCAA = {
+//        "symbol":"MCAA",
+//        "name":"Mountain & Co. I Acquisition Corp.",
+//        "currency":"USD",
+//        "stockExchange":"NASDAQ Global Market",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const FWAA = {
+//        "symbol":"FWAA",
+//        "name":"Fifth Wall Acquisition Corp. I",
+//        "currency":"USD","stockExchange":"Nasdaq Capital Market",
+//        "exchangeShortName":"NASDAQ"
+//    };
+   
+//     const PRAA = {
+//        "symbol":"PRAA",
+//        "name":"PRA Group, Inc.",
+//        "currency":"USD",
+//        "stockExchange":"NASDAQ Global Select","exchangeShortName":"NASDAQ"}
+
+
+
+// // window.onload = async function() {
+
+// //     console.log("stockClicked", urlParams)
+// // }
+// //     const params = new URLSearchParams(location.search);
+// //     const symbol = params.get('symbol');
+// //     const compInfo = new CompanyInfo(document.getElementById('compInfo'), symbol);
+// //     await compInfo.onload();
+// //     await compInfo.addChart();
+// // }();
+
--- script.js
+++ script.js
@@ -1,37 +1,86 @@
+marqueeStocks();
 
-// const url = 'https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=AA&limit=10&exchange=NASDAQ'
-
-const searchButton = document.getElementById('searchButton')
+const searchButton = document.getElementById('searchButton');
 
 searchButton.addEventListener('click', () => {
-    searchResults();
+    document.getElementById("tableResults").innerHTML = '';
+    searchResults()
     addSpinner();
 });
 
+async function searchResults() {
+    const symbol = document.getElementById('searchTerm').value;
+    const url = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query="+ symbol + "&limit=10&exchange=NASDAQ"
+    const response = await fetch(url)
+    const result = await response.json();
 
+   
 
-function searchResults() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-
-        stopSpinner();
-        
-        let text = this.response;
-        let result = "";
-        
-        JSON.parse(text).forEach(value => result += "<a href='company.html?symbol=" + value.symbol +"'>" + value.name + "  " + '(' + value.symbol + ') </a>  <hr>')
-        
-
-        document.getElementById("tableResults").innerHTML = result;
-        }
-    };
-
+    // companyResults();
+    stopSpinner();
     
+    const tableResults = document.getElementById("tableResults");
+    let searchResults = result;
+    try {
+        for (let i = 0; i < searchResults.length; i++) {
+            let currentResult = searchResults[i];
+            tableResults.innerHTML += ('<img src="https://financialmodelingprep.com/images-New-jpg/' + currentResult.symbol + '.jpg"> ' + "<a href='company.html?symbol=" + currentResult.symbol +"'>"  + currentResult.name  + ' </a>' + '(' + currentResult.symbol + ')');
 
-    xmlhttp.open("GET", "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=AA&limit=10&exchange=NASDAQ", true);
-    xmlhttp.send();
+            const url2 = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/" + currentResult.symbol;
+            const response2 = await fetch(url2);
+            const result2 = await response2.json();
+            let compResults = result2;
+            
+            tableResults.innerHTML += " (" + compResults.profile.changesPercentage + "%)" + '<hr>';
+            // const priceSpan = document.getElementById("priceSpan")
+            // if (compResults.profile.changesPercentage >= 0) {
+            //     tableResults.innerHTML = ' (+' + compResults.profile.changesPercentage[i] + '%)' + '<hr>';
+            //     tableResults.style.color = "green";
+            // } else {
+            //     tableResults.innerHTML = ' (' + compResults.profile.changesPercentage[i] + '%)' + '<hr>';
+            //     tableResults.style.color = "red";
+            // }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+    // async function companyResults() {
+    //     const url2 = "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/";
+    //     const response2 = await fetch(url2 + currentResult.symbol);
+    //     console.log(response2)
+    //     const result2 = await response2.json();
+    //     let compResults = result2;
+    //             const tableResults = document.getElementById("tableResults");
+
+    //     try {
+    //         for (let i = 0; i < compResults.length; i++) {
+    //             let currentResult = compResults[i];
+    //             tableResults.innerHTML() += '$' + currentResult.profile.price;
+    //         }
+    //     } catch (error) {
+    //     console.error(error);
+    //     }
+    // }
 }
+
+
+            
+async function marqueeStocks() {
+    const url = "https://financialmodelingprep.com/api/v3/stock/list?apikey=63ce29e954a5731b77ccf40f124b41b2"
+    const response = await fetch(url);
+    const result = await response.json();
+    const marqueeContainer = document.getElementById("marqueeNav");
+    let marqueeResult = result;
+    try {
+        for (let i = 45000; i < marqueeResult.length; i++) {
+            let currentResult = marqueeResult[i];
+            marqueeContainer.innerHTML += ( currentResult.symbol + ': ' + '<span>' + '$' + currentResult.price + '</span>' + ' ');        
+        }
+    } catch (error) {
+        console.error(error);
+    }  
+}      
 
 function addSpinner() {
     document.getElementsByClassName('loader')[0].style.display = 'block';
@@ -44,3 +93,4 @@ function stopSpinner() {
 window.onload = function() {
     document.getElementById('searchTerm').value = '';
 };
+
